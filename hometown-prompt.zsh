63977515e5e870532fa213f6bc25871881e39109
Henry Bley-Vroman <olets@olets.dev>
2021-12-27 15:23:42 -1000

    feat(theme): add

---
+++ hometown-prompt.zsh
@@ -0,0 +1,72 @@
+#!/usr/bin/env zsh
+
+# Hometown
+# https://github.com/olets/hometown-prompt
+# Copyright (©) 2021 Henry  Bley-Vroman
+
+_hometown-prompt_prompt_git_prompt() {
+  emulate -L zsh
+
+  local git_prompt=
+
+  git_prompt+='$GIT_PROMPT_KIT_REF'
+  git_prompt+='${GIT_PROMPT_KIT_SHOW_EXTENDED_STATUS:+$GIT_PROMPT_KIT_STATUS_EXTENDED}'
+  # Add a space after the extended Git prompt if there's a Git status or a Git action
+  git_prompt+='${${GIT_PROMPT_KIT_SHOW_EXTENDED_STATUS:+$GIT_PROMPT_KIT_STATUS_EXTENDED}:+${${GIT_PROMPT_KIT_STATUS:-$GIT_PROMPT_KIT_ACTION}:+ }}'
+  git_prompt+='$GIT_PROMPT_KIT_STATUS'
+  # Add a space after the Git prompt if there's a Git action
+  git_prompt+='${GIT_PROMPT_KIT_STATUS:+${GIT_PROMPT_KIT_ACTION:+ }}'
+  git_prompt+='$GIT_PROMPT_KIT_ACTION'
+
+  'builtin' 'echo' $git_prompt
+}
+
+_hometown-prompt_prompt_build_prompt() {
+  emulate -L zsh
+
+  local prompt=
+
+  # Blank line after result of previous command
+  prompt+=$'\n'
+
+  # User and host
+  prompt+='${GIT_PROMPT_KIT_USERHOST:+$GIT_PROMPT_KIT_USERHOST }'
+
+  # Time
+  prompt+=$'%* '
+
+  # Custom (CWD by default)
+  prompt+='$GIT_PROMPT_KIT_CUSTOM'
+
+  # Git
+  # add a space if GIT_PROMPT_KIT_NO_LINEBREAK_BEFORE_GIT_REF, otherwise add a linebreak
+  prompt+='${GIT_PROMPT_KIT_HEAD:+${${GIT_PROMPT_KIT_NO_LINEBREAK_BEFORE_GIT_REF:+ }:-\n}}'
+  prompt+=$(_hometown-prompt_prompt_git_prompt)
+
+  # Prompt character
+  prompt+=$'\n'
+  prompt+='${GIT_PROMPT_KIT_CHAR:+$GIT_PROMPT_KIT_CHAR }'
+
+  'builtin' 'echo' $prompt
+}
+
+_hometown-prompt_prompt_init() {
+  emulate -L zsh
+
+  local dir
+
+  if ! (( $# )); then
+    'builtin' 'printf' Hometown could not be initialized
+  fi
+
+  dir=$1
+
+  GIT_PROMPT_KIT_GITSTATUS_FUNCTIONS_SUFFIX=__hometown-prompt_prompt
+  GIT_PROMPT_KIT_GITSTATUSD_INSTANCE_NAME=HOMETOWN_PROMPT
+
+  'builtin' 'source' $dir/git-prompt-kit/git-prompt-kit.zsh
+
+  PROMPT=$(_hometown-prompt_prompt_build_prompt)
+}
+
+_hometown-prompt_prompt_init ${0:A:h}
