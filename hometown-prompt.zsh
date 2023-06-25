e6e75e61266f88a941c7dcf531ca88ae69d3a6a6
Henry Bley-Vroman <olets@olets.dev>
2023-06-24 14:42:01 -1000

    chore(prompt): always wrap variable names in curly brackets

--- hometown-prompt.zsh
+++ hometown-prompt.zsh
@@ -16,18 +16,18 @@ _hometown_prompt_git_prompt() {
   git_prompt+='$GIT_PROMPT_KIT_REF'
 
   if (( HOMETOWN_PROMPT_SHOW_EXTENDED_STATUS )); then
-    git_prompt+='$GIT_PROMPT_KIT_STATUS_EXTENDED'
+    git_prompt+='${GIT_PROMPT_KIT_STATUS_EXTENDED}'
 
     # Add a space after the extended Git prompt if there's an extended Git status and either a Git status or a Git action
     git_prompt+='${GIT_PROMPT_KIT_STATUS_EXTENDED:+${${GIT_PROMPT_KIT_STATUS:-$GIT_PROMPT_KIT_ACTION}:+ }}'
   fi
 
-  git_prompt+='$GIT_PROMPT_KIT_STATUS'
+  git_prompt+='${GIT_PROMPT_KIT_STATUS}'
 
   # Add a space after the Git status if there's a Git action
   git_prompt+='${GIT_PROMPT_KIT_STATUS:+${GIT_PROMPT_KIT_ACTION:+ }}'
 
-  git_prompt+='$GIT_PROMPT_KIT_ACTION'
+  git_prompt+='${GIT_PROMPT_KIT_ACTION}'
 
   'builtin' 'echo' $git_prompt
 }
@@ -50,7 +50,7 @@ _hometown_prompt_build_prompt() {
   prompt+='${HOMETOWN_PROMPT_CUSTOM:+$HOMETOWN_PROMPT_CUSTOM }'
 
   # Working directory
-  prompt+='$GIT_PROMPT_KIT_WORKDIR'
+  prompt+='${GIT_PROMPT_KIT_WORKDIR}'
 
   # Git
   # add a space if GIT_PROMPT_KIT_NO_LINEBREAK_BEFORE_GIT_REF, otherwise add a linebreak
